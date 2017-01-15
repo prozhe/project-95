682f07e2f43078645ed843d61cdfee20e55e7b7f
Unknown <msa1376.sa@gmail.com>
2017-01-15 20:50:47 +0330

    xclear function is added

--- main.c
+++ main.c
@@ -3,10 +3,49 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-//#include <conio.h>
+#include <conio.h>
 #include <dirent.h>
 
-int main()
+void xclear(void);
+
+int main(){}
+
+void xclear(void)
 {
+	int x = 0, y = 0; char ch;
+
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsole, &coninfo);
+	coninfo.dwCursorPosition.Y = 0;
+	coninfo.dwCursorPosition.X = 0;
+	SetConsoleCursorPosition(hConsole, coninfo.dwCursorPosition);
+
+	int counter, i;
+
+	for (counter = 0; counter < 2000; counter++)
+	{
+		GetConsoleScreenBufferInfo(hConsole, &coninfo);
+		coninfo.dwCursorPosition.Y = y;
+		coninfo.dwCursorPosition.X = x;
+		SetConsoleCursorPosition(hConsole, coninfo.dwCursorPosition);
+jump:
+			if (ch = fgetc(stdout) == '\n')
+		{
+			printf("%c", NULL);
+			x = 0;
+			y++;
+		}
+		else
+		{ 
+			printf("%c", NULL);
+			x++;
+		}
+	}
+
+	GetConsoleScreenBufferInfo(hConsole, &coninfo);
+	coninfo.dwCursorPosition.Y = 0;
+	coninfo.dwCursorPosition.X = 0;
+	SetConsoleCursorPosition(hConsole, coninfo.dwCursorPosition);
 
 }
