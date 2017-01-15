3010a15b68d8377ab3e949ac738e9a90e2810435
Unknown <dayyanilisani@yahoo.com>
2017-01-15 21:45:25 +0330

    Calender Function Added

--- main.c
+++ main.c
@@ -5,11 +5,91 @@
 #include <unistd.h>
 #include <conio.h>
 #include <dirent.h>
-
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
 void xclear(void);
-
+int isLeapYear( int year );        /* True if leap year */
+int leapYears( int year );         /* The number of leap year */
+int todayOf( int y, int m, int d); /* The number of days since the beginning of the year */
+long days( int y, int m, int d);   /* Total number of days */
+void calendar(int y, int m);       /* display calendar at m y */
+int cal(void);
 int main(){}
+int cal(void){
+    int year,month;
 
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    char s[64];
+    char o[64];
+    strftime(s, sizeof(s), "%m", tm);
+    char *stringPtr;
+    char *date = s;
+    int d,c;
+    d = strtod( date, &stringPtr );
+    strftime(o, sizeof(o), "%Y", tm);
+    char *yea = o;
+    c = strtod( yea, &stringPtr );
+
+    calendar(c, d);
+	system("pause");
+    return 0;
+}
+
+int isLeapYear( int y ) /* True if leap year */
+{
+    return(y % 400 == 0) || ((y % 4 == 0) && (y % 100 != 0));
+}
+
+int leapYears( int y ) /* The number of leap year */
+{
+    return y/4 - y/100 + y/400;
+}
+
+int todayOf( int y, int m, int d) /* The number of days since the beginning of the year */
+{
+    static int DayOfMonth[] =
+        { -1/*dummy*/,0,31,59,90,120,151,181,212,243,273,304,334};
+
+    return DayOfMonth[m] + d + ((m>2 && isLeapYear(y))? 1 : 0);
+}
+
+long days( int y, int m, int d) /* Total number of days */
+{
+    int lastYear;
+
+    lastYear = y - 1;
+
+    return 365L * lastYear + leapYears(lastYear) + todayOf(y,m,d);
+}
+
+void calendar(int y, int m) /* display calendar at m y */
+{
+    const char *NameOfMonth[] = { NULL/*dummp*/,
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+    char Week[] = "Su Mo Tu We Th Fr Sa";
+    int DayOfMonth[] =
+        { -1/*dummy*/,31,28,31,30,31,30,31,31,30,31,30,31 };
+    int weekOfTopDay;
+    int i,day;
+
+    weekOfTopDay = days(y, m, 1) % 7;
+    if(isLeapYear(y))
+        DayOfMonth[2] = 29;
+    printf("\n     %s %d\n%s\n", NameOfMonth[m], y, Week);
+
+    for(i=0;i<weekOfTopDay;i++)
+        printf("   ");
+    for(i=weekOfTopDay,day=1;day <= DayOfMonth[m];i++,day++){
+        printf("%2d ",day);
+        if(i % 7 == 6)
+            printf("\n");
+    }
+    printf("\n");
+}
 void xclear(void)
 {
 	int x = 0, y = 0; char ch;
